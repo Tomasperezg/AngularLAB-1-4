c56ff7fc3f44b83a0698b49ce539d60831ea678b
tomas <perezgantomas@gmail.com>
2018-05-21 20:35:11 -0400

    filter added

--- controller.js
+++ controller.js
@@ -2,10 +2,40 @@
 {
   function ToDoController(){
     let vm = this;
-    vm.nums = ["Walk the Dog","Do homework","Clean the car"];
+    vm.nums = [
+      {nums: "do homework", className: "Complete"},
+      {nums: "clean the car", className: "Complete"},
+      {nums: "walk the dog", className: "Complete"},
+      {nums: "work in lab", className: "Complete"}
+    ];
+
   }
 
-  angular
-  .module("ToDoApp")
-  .controller("ToDoController", ToDoController);
+var changeText = angular.module("ToDoApp", []);
+changeText.controller('GoDoController',['$scope', '$http', function($scope, $http){//
+  $scope.class1 = false;
+  $scope.clicked = true;
+  $scope.click = true;
+
+  $scope.ChangeStyle = function(){
+    $scope.class1 = true;
+    $scope.clicked = false;
+  };
+  $scope.removeItem = function(){
+    $scope.click = false;
+    $scope.this.splice(this.nums);
+  };
+  $scope.addItems = function(input){
+    $scope.click = false;
+    let array = {
+      nums: input,
+      className: ""
+    }
+    nums.push(array);
+  };
+
+}]);
+angular
+.module("ToDoApp")
+.controller("ToDoController", ToDoController);
 }
